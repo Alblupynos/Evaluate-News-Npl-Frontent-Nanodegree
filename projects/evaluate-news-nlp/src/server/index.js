a052fbd65982e8dd0a0cdf92b753df9891f18c20
Albina <alblupynos@gmail.com>
2023-10-12 16:34:23 +0300

    Add post to server.

--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,6 +1,9 @@
-var path = require('path')
+const path = require('path')
 const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+const dotenv = require('dotenv');
+dotenv.config();
+const apiKey = process.env.API_KEY;
+const baseUrl = 'https://api.meaningcloud.com/sentiment-2.1';
 
 const app = express()
 
@@ -10,14 +13,30 @@ console.log(__dirname)
 
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
-    // res.sendFile(path.resolve('src/client/views/index.html'))
+})
+
+app.post('/api', async(req, res)=> {
+    const url = `${baseUrl}?key=${apiKey}&url=${req.body.url}&lang=en`;
+    const response = await fetch(url)
+    try {
+        const Data = await response.json();
+        let agreement = Data.agreement;
+        let subjectivity = Data.subjectivity;
+
+        let data = {
+            agreement,
+            subjectivity,
+        }
+
+        res.send(data);
+    }
+    catch (error) {
+        console.log("error", error);
+    }
+
 })
 
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
     console.log('Example app listening on port 8081!')
-})
-
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
 })
